f7e9825f1f816684e488ded4226b527abaaa192f
jvcmtr <jvictorcmtr@gmail.com>
2024-09-12 08:09:25 -0300

    changed main.py

--- settup/main.py
+++ settup/main.py
@@ -1,13 +1,41 @@
 import os
 txt = ""
-styleSheet = "<link rel=\"stylesheet\" href=\"styles.css\" />\n"
+template = """
+<link rel="stylesheet" href="styles.css" />
+
+<div class="nav-container">
+  <h1>@TITLE</h1>
+  <nav>
+    <ul class="nav-list">
+      <li><a href="exercicio1.html">Exercício 1</a></li>
+      <li><a href="exercicio2.html">Exercício 2</a></li>
+      <li><a href="exercicio3.html">Exercício 3</a></li>
+      <li><a href="exercicio4.html">Exercício 4</a></li>
+      <li><a href="exercicio5.html">Exercício 5</a></li>
+      <li><a href="exercicio6.html">Exercício 6</a></li>
+      <li><a href="exercicio7.html">Exercício 7</a></li>
+      <li><a href="exercicio8.html">Exercício 8</a></li>
+      <li><a href="exercicio9.html">Exercício 9</a></li>
+      <li><a href="exercicio10.html">Exercício 10</a></li>
+      <li><a href="exercicio11.html">Exercício 11</a></li>
+      <li><a href="exercicio12.html">Exercício 12</a></li>
+      <li><a href="exercicio13.html">Exercício 13</a></li>
+      <li><a href="exercicio14.html">Exercício 14</a></li>
+      <li><a href="exercicio15.html">Exercício 15</a></li>
+      <li><a href="exercicio16.html">Exercício 16</a></li>
+    </ul>
+  </nav>
+</div>
+<p>
+"""
+
 tarefa = 0
 
 
 def escrever_arquivo(path, str, type='w'):
     try:
         os.makedirs(os.path.dirname(path), exist_ok=True)
-        with open(path, type) as file:
+        with open(path, type, encoding='utf8') as file:
             file.write(str)
         print(f" atualizado o arquivo : {path}")
     except Exception:
@@ -16,24 +44,23 @@ def escrever_arquivo(path, str, type='w'):
 
 def ler_arquivo(path):
     try:
-        f = open(path, "r")
+        f = open(path, "r",  encoding="utf8")
         return "" + f.read()
     except:
         print(f"@ERRO ao ler o arquivo: {path}")
         return ""
 
-def initTarefa(str):
+def initTarefa(str, title):
     global txt, tarefa
     tarefa = int(str)
-    txt = "<p> "
+    txt = template.replace("@TITLE", title)
 
 def endTarefa():
     global txt, tarefa, styleSheet
     if tarefa == 0 :
         return
     txt += " </p>"
-    txt = styleSheet + ler_arquivo(f'./exercicio{tarefa}.html') + txt
-    escrever_arquivo(f'./exercicio{tarefa}.html', txt)
+    escrever_arquivo(f'./template/exercicio{tarefa}.html', txt)
 
 
 def main():
@@ -42,7 +69,7 @@ def main():
     for line in a.split('\n'):
         if 'Tarefa' in line :
             endTarefa()
-            initTarefa(line[6:9].strip())
+            initTarefa(line[6:9].strip(), line)
         else:
             txt += line + '\n'
     endTarefa()
